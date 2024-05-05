78603a0c6d409a418f42fde7f1f2f79c98a0f03f
henrybae10 <henrybae10@gmail.com>
2024-05-05 10:36:39 +0900

    clone kakao talk

--- js/background.js
+++
@@ -1,8 +0,0 @@
-const backgroundImg = ["1.jpeg", "2.jpeg", "3.jpeg", "4.jpeg"];
-const chosenBackgroundImg =
-	backgroundImg[Math.floor(Math.random() * backgroundImg.length)];
-
-const bodyElem = document.body;
-bodyElem.style.backgroundImage = `url(img/${chosenBackgroundImg})`;
-bodyElem.style.backgroundSize = "100vw 100vh";
-bodyElem.style.backgroundRepeat = "no-repeat";
